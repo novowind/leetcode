8628be61a309af35b5a63581387c1d16e6372d90
novowind <novowind@gmail.com>
2022-04-08 16:44:25 +0800

    init

---
+++ cc_bazel/two-sum/traverse/main/solution.cc
@@ -0,0 +1,8 @@
+#include "solution.h"
+
+using namespace std;
+
+vector<int> Solution::twoSum(vector<int>& nums, int target) {
+  vector<int> a;
+  return a;
+}
---
+++ cc_bazel/two-sum/traverse/main/traverse.cc
@@ -0,0 +1,14 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+
+#include "solution.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  Solution s;
+
+  std::cout << "over" << std::endl;
+  return 0;
+}
